efbd962cdde11e16e5e8b692d913ce826bb5dffd
aiyane <2310091880@qq.com>
2018-04-07 22:56:46 +0800

    parse

---
+++ parse.py
@@ -0,0 +1,369 @@
+#!/usr/bin/env/python3
+# -*- coding: utf-8 -*-
+# parse.py
+import re
+
+
+# 终结符号
+LB = 'LB'  # (
+RB = 'RB'  # )
+LCB = 'LCB'  # {
+RCB = 'RCB'  # }
+LSB = 'LSB'  # [
+RSB = 'RSB'  # ]
+ASSIGN = 'ASSIGN'  # =
+COLON = 'COLON'  # :
+COMMA = 'COMMA'  # ,
+INT = 'INT'
+FLOAT = 'FLOAT'
+ID = 'ID'
+TABEND = 'TABEND'
+END = 'END'
+DEF = 'DEF'
+IF = 'IF'
+FOR = 'FOR'
+IN = 'IN'
+NOT = 'NOT'
+AND = 'AND'
+OR = 'OR'
+ADD = 'ADD'  # +
+SUB = 'SUB'  # -
+MUL = 'MUL'  # *
+DIV = 'DIV'  # //
+REAL_DIV = 'REAL_DIV'  # /
+TRUE = 'TRUE'
+FALSE = 'FALSE'
+GT = 'GT'  # >
+GE = 'GE'  # >=
+LT = 'LT'  # <
+LE = 'LE'  # <=
+EQ = 'EQ'  # ==
+NE = 'NE'  # !=
+IS = 'IS'
+RET = 'RET'
+ELIF = 'ELIF'
+ELSE = 'ELSE'
+WHILE = 'WHILE'
+BREAK = 'BREAK'
+CONTINUE = 'CONTINUE'
+EOF = 'EOF'
+
+
+class Token(object):
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        """
+        Examples:
+            Token(INTEGER, 3)
+            Token(PLUS, '+')
+            Token(MUL, '*')
+        """
+        return 'Token({type}, {value})'.format(
+            type=self.type,
+            value=repr(self.value)
+        )
+
+    def __repr__(self):
+        return self.__str__()
+
+
+# 关键字
+KEY_WORDS = {
+    'is': Token('IS', 'IS'),
+    'continue': Token('CONTINUE', 'CONTINUE'),
+    'break': Token('BREAK', 'BREAK'),
+    'while': Token('WHILE', 'WHILE'),
+    'for': Token('FOR', 'FOR'),
+    'return': Token('RET', 'RET'),
+    'if': Token('IF', 'IF'),
+    'elif': Token('ELIF', 'ELIF'),
+    'else': Token('ELSE', 'ELSE'),
+    'def': Token('DEF', 'DEF'),
+    'not': Token('NOT', 'NOT'),
+    'or': Token('OR', 'OR'),
+    'and': Token('AND', 'AND'),
+    'True': Token('TRUE', 'TRUE'),
+    'False': Token('FALSE', 'FALSE'),
+}
+
+
+class Lexer(object):
+    def __init__(self, program):
+        self.tab_level = 0
+        self.program = program
+        self.stack = []
+        pass
+
+    # 一行的分词
+    def tokenize_line(self, line):
+        cur_tab_level = self.count_tab_level(line)
+        if cur_tab_level > self.tab_level:
+            self.tab_level = cur_tab_level
+        elif cur_tab_level < self.tab_level:
+            while cur_tab_level != self.tab_level:
+                self.tab_level -= 1
+                yield '$'
+
+        for m in re.finditer(r'\'\'\'|"""|".*?"|\'.*?\'|//?|[-+*()[\]{}:,;#]|[><=]=?|!=|[A-Za-z_]+\d*|\d+(?:\.\d*)?', line):
+            tok = m.group(0)
+            yield tok
+
+    # 计算缩进级别
+    def count_tab_level(self, line):
+        tab_level = 0
+        while line[tab_level*4:(tab_level+1)*4] == ' '*4:
+            tab_level += 1
+        return tab_level
+
+    def _id(self, tok):
+        token = KEY_WORDS.get(tok, Token(ID, tok))
+        return token
+
+    def number(self, tok):
+        return Token(FLOAT, tok) if '.' in tok else Token(INT, 'tok')
+
+    def error(self):
+        raise Exception('Invalid character')
+
+    def tokenize(self):
+        jump = False
+        for line in self.program:
+            for tok in self.tokenize_line(line):
+                if tok in ("'''", '"""'):
+                    jump = not jump
+                    break
+                if tok == "#" or jump:
+                    break
+                if re.match(r'[A-Za-z_]+\d*', tok):
+                    yield self._id(tok)
+                if re.match(r'\d+(?:\.\d*)?', tok):
+                    yield self.number(tok)
+                if tok == ':':
+                    yield Token(COLON, ':')
+                if tok == '=':
+                    yield Token(ASSIGN, '=')
+                if tok == '==':
+                    yield Token(EQ, '==')
+                if tok == '>':
+                    yield Token(GT, '>')
+                if tok == '<':
+                    yield Token(LT, '<')
+                if tok == '>=':
+                    yield Token(GE, '>=')
+                if tok == '<=':
+                    yield Token(LE, '<=')
+                if tok == '!=':
+                    yield Token(NE, '!=')
+                if tok == '(':
+                    yield Token(LB, '(')
+                if tok == ')':
+                    yield Token(RB, ')')
+                if tok == '{':
+                    yield Token(LCB, '{')
+                if tok == '}':
+                    yield Token(RCB, '}')
+                if tok == '[':
+                    yield Token(LSB, '[')
+                if tok == ']':
+                    yield Token(RSB, ']')
+                if tok == '+':
+                    yield Token(ADD, '+')
+                if tok == '-':
+                    yield Token(SUB, '-')
+                if tok == '*':
+                    yield Token(MUL, '*')
+                if tok == '/':
+                    yield Token(REAL_DIV, '/')
+                if tok == '//':
+                    yield Token(DIV, '//')
+                if tok == ',':
+                    yield Token(COMMA, ',')
+                if tok == ';':
+                    yield Token(END, ';')
+                if tok == '$':
+                    yield Token(TABEND, None)
+                if tok == ';':
+                    continue
+                self.error()
+            yield Token(END, None)
+        yield Token(EOF, None)
+
+
+class AST(object):
+    pass
+
+
+class BinOp(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+
+class Num(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+
+class Assign(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+
+class Var(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+
+class Block(AST):
+    def __init__(self):
+        self.tokens = []
+
+
+class CallFunc(AST):
+    def __init__(self, func, *attrs):
+        self.token = func
+        self.attrs = attrs
+
+
+class Parser(object):
+    def __init__(self, lexer):
+        self.tokens = lexer.tokenize()
+        self.cur_token = next(self.tokens)
+
+    def error(self):
+        raise Exception('Invalid syntax')
+
+    def eat(self, token_type):
+        if self.cur_token.type == token_type:
+            self.cur_token = next(self.tokens)
+        else:
+            self.error()
+
+    def build_ld(self):
+        # 列表解析, 字典解析
+        pass
+
+    def stmt(self):
+        # 赋值语句或call_func
+        var_name = Var(self.cur_token)
+        self.eat(ID)
+        if self.cur_token.type == ASSIGN:
+            op = self.cur_token
+            self.eat(ASSIGN)
+            if self.cur_token.type in (ID, FLOAT, INT):
+                left = self.factor()
+                right = self.expr(left)
+            elif self.cur_token.type in (LSB, LCB):
+                right = self.build_ltd()
+            else:
+                self.error()
+            node = Assign(var_name, op, right)
+        elif self.cur_token.type == LB:
+            node = self.call_func(var_name)
+        else:
+            self.error()
+        return node
+
+    def call_func(self, func_name):
+        # 函数调用
+        self.eat(LB)
+        attrs = []
+        while self.cur_token.type in (ID, INT, FLOAT):
+            attrs.append(self.factor())
+            if self.cur_token.type == COMMA:
+                self.eat(COMMA)
+        self.eat(RB)
+        return CallFunc(func_name, attrs)
+
+    def expr(self, left):
+        # 表达式, 也可以是函数调用
+        while True:
+            if self.cur_token.type == END:
+                self.eat(END)
+                return left
+            if self.cur_token.type == RB:
+                return left
+            if self.cur_token.type in (ADD, SUB):
+                op = self.cur_token
+                self.eat(op.type)
+                right = self.term(self.factor())
+                left = BinOp(left, op, right)
+            elif self.cur_token.type in (MUL, DIV, REAL_DIV):
+                left = self.term(left)
+            elif self.cur_token.type == LB:
+                node = self.call_func(left)
+                return node
+            else:
+                self.error()
+        return left
+
+    def factor(self):
+        # NUM或ID
+        node = self.cur_token
+        if node.type == ID:
+            self.eat(ID)
+            return Var(node)
+        if node.type == INT:
+            self.eat(INT)
+            return Num(node)
+        if node.type == FLOAT:
+            self.eat(FLOAT)
+            return Num(node)
+        if node.type == LB:
+            self.eat(LB)
+            left = self.factor()
+            node = self.expr(left)
+            self.eat(RB)
+            return node
+        self.error()
+
+    def term(self, left):
+        # 乘除
+        if self.cur_token.type in (ADD, SUB):
+            return left
+        if self.cur_token.type in (MUL, DIV, REAL_DIV):
+            op = self.cur_token
+            self.eat(op.type)
+            right = self.factor()
+            return BinOp(left, op, right)
+        self.error()
+
+    def frame(self):
+        # 函数定义
+        pass
+
+    def block(self):
+        node = Block()
+        while True:
+            if self.cur_token.type == EOF:
+                break
+            elif self.cur_token.type == ID:
+                node.tokens.append(self.stmt())
+            elif self.cur_token.type == IF:
+                node.tokens.append(self.if_expr())
+            elif self.cur_token.type == DEF:
+                node.tokens.append(self.frame())
+            else:
+                self.error()
+        return node
+
+    def parse(self):
+        node = self.block()
+        return node
+
+
+def main():
+    with open('test.py', "r", encoding="utf8") as f:
+        pass
+
+
+if __name__ == '__main__':
+    main()
--- test.py
+++ test.py
@@ -3,17 +3,40 @@
 # test.py
 
 
+# 暂不支持类
+# class Test(object):
+#     def __init__(self, name, pwd):
+#         self.name = name
+#         self.pwd = pwd
+
+#     def say(self):
+#         print("Hello", self.name)
+b1 = [1, 2, 3, 4, 5]
+a1 = 10
+
+
+def say(a1, b1):
+
+    if a1 >= 10:
+        print("hello", a1)
+    for i in b1:
+        print(i)
+
+
 def hello(a, b):
-    # 还不支持
-    # a += 1
-    # b -= 1
+    """
+    还不支持
+    a += 1
+    b -= 1
+    """
+    say(a1, b1)
     a = a + 1
     b = b - 1
     return a+b
 
 
 a = 10
-b = 5
+b = 5.5
 c = a - b
 
 print(hello(a, c))
