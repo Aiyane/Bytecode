cdab8bb549e6c7d5047b8838d4f8cd76599a23c7
aiyane <2310091880@qq.com>
2018-04-09 13:28:40 +0800

    数字

--- parse.py
+++ parse.py
@@ -10,7 +10,9 @@ LSB = 'LSB'  # [
 RSB = 'RSB'  # ]
 ASSIGN = 'ASSIGN'  # =
 COLON = 'COLON'  # :
+CEMI = 'CEMI'  # ;
 COMMA = 'COMMA'  # ,
+DOT = 'DOT'  # .
 ADD = 'ADD'  # +
 SUB = 'SUB'  # -
 MUL = 'MUL'  # *
@@ -31,6 +33,9 @@ EQ = 'EQ'  # ==
 NE = 'NE'  # !=
 EOF = 'EOF'
 INT = 'INT'
+OINT = 'OINT'
+BINT = 'BINT'
+XINT = 'XINT'
 FLOAT = 'FLOAT'
 ID = 'ID'
 STR = 'STR'
@@ -130,6 +135,10 @@ KEY_WORDS = {
 }
 
 
+class CharacterError(Exception):
+    pass
+
+
 class Lexer(object):
     def __init__(self, text):
         self.text = text
@@ -172,19 +181,8 @@ class Lexer(object):
             self.advance()
         self.advance()
 
-    # # 一行的分词
-    # def tokenize_line(self, line):
-    #     cur_tab_level = self.count_tab_level(line)
-    #     if cur_tab_level != self.tab_level:
-    #         self.tab_level = cur_tab_level
-    #         yield self.tab_level
-
-    #     for m in re.finditer(r'\'\'\'|"""|".*?"|\'.*?\'|//?|[-+*()[\]{}:,;#]|[><=]=?|!=|[A-Za-z_]+\d*|\d+(?:\.\d*)?', line):
-    #         tok = m.group(0)
-    #         yield tok
-
-    # 计算缩进级别
     def count_tab_level(self, line):
+        # 计算缩进级别
         tab_level = 0
         while line[tab_level*4:(tab_level+1)*4] == ' '*4:
             tab_level += 1
@@ -490,7 +488,7 @@ class Lexer(object):
         if (cur_char in ("B", "b") and next_char in ("R", "r") or
                 cur_char in ("R", "r") and next_char in ("B", "b")):
             if next_next_char in ('"', "'"):
-                return self.bytesliteral
+                return self.bytesliteral()
 
         if cur_char in ("r", "R", "u", "U", "f", "F") and next_char in ("'", '"'):
             return self.stringliteral()
@@ -501,7 +499,7 @@ class Lexer(object):
                 return self.stringliteral()
 
         result = ''
-        while cur_char != ' ' and cur_char.isalnum() or cur_char == '_':
+        while self.current_char is not None and cur_char.isalnum() or cur_char == '_':
             result += self.current_char
             self.advance()
 
@@ -539,6 +537,7 @@ class Lexer(object):
             result = self.current_char
             self.advance()
             result += self.current_char
+            self.advance()
 
             has_error = True
             while True:
@@ -565,26 +564,188 @@ class Lexer(object):
     def octinteger(self):
         # "0" ("o" | "O") (["_"] octdigit)+
         return self.baseinteger(("o", "O"), self.octdigit)
-    
+
     def bininteger(self):
         # "0" ("b" | "B") (["_"] bindigit)+
         return self.baseinteger(("b", "B"), self.bindigit)
 
     def decinteger(self):
         # nonzerodigit (["_"] digit)* | "0"+ (["_"] "0")*
-        result = self.nonzerodigit()
+        result = ''
 
-        while True:
+        if self.current_char == "0":
+            while self.current_char == "0":
+                result += "0"
+                self.advance()
+
             while True:
-                pass
-    
+                if self.current_char == '_':
+                    result += '_'
+                    self.advance()
 
+                if self.current_char == "0":
+                    result += "0"
+                    self.advance()
+                else:
+                    break
 
-    def number(self, tok):
-        return Token(FLOAT, float(tok)) if '.' in tok else Token(INT, int(tok))
+            return result
+
+        result = self.nonzerodigit()
+        if result:
+            has_error = False
+            while True:
+                if self.current_char == '_':
+                    has_error = True
+                    result += self.current_char
+                    self.advance()
+
+                temp_num = self.digit()
+                if temp_num:
+                    result += temp_num
+                    has_error = False
+                else:
+                    break
+
+            if has_error:
+                self.error()
+
+            return result
+
+        self.error()
+
+    def integer(self):
+        # decinteger | bininteger | octinteger | hexinteger
+        if self.current_char.isdigit():
+            if self.current_char == "0":
+                if self.peek() in ("o", "O"):
+                    return Token(OINT, int(self.octinteger(), 8))
+
+                if self.peek() in ("b", "B"):
+                    return Token(BINT, int(self.bininteger(), 2))
+
+                if self.peek() in ("x", "X"):
+                    return Token(XINT, int(self.hexinteger(), 16))
+
+            return Token(INT, int(self.decinteger()))
+
+        self.error()
+
+    def digitpart(self):
+        # digit (["_"] digit)*
+        if self.current_char.isdigit():
+            result = self.digit()
+
+            while True:
+                if self.current_char == '_':
+                    result += '_'
+                    self.advance()
+
+                if self.current_char.isdigit():
+                    result += self.digit()
+                else:
+                    break
+
+            return result
+
+        self.error()
+
+    def fraction(self):
+        # "." digitpart
+        if self.current_char == ".":
+            result = '.'
+            self.advance()
+            result += self.digitpart()
+            return result
+
+        self.error()
+
+    def exponent(self):
+        # ("e" | "E") ["+" | "-"] digitpart
+        if self.current_char in ("e", "E"):
+            result = self.current_char
+            self.advance()
+
+            if self.current_char in ("+", "-"):
+                result += self.current_char
+                self.advance()
+
+            result += self.digitpart()
+            return result
+
+        self.error()
+
+    def pointfloat(self):
+        # [digitpart] fraction | digitpart "."
+        result = ''
+        if self.current_char.isdigit():
+            result = self.digitpart()
+
+            if self.current_char == '.':
+                if self.peek().isdigit():
+                    result += self.fraction()
+                    return result
+
+                result += '.'
+                self.advance()
+                return result
+            self.error()
+
+        if self.current_char == '.':
+            return self.fraction()
+
+        self.error()
+
+    def exponentfloat(self):
+        # (digitpart | pointfloat) exponent
+        pos = self.pos
+        try:
+            result = self.pointfloat()
+        except CharacterError:
+            self.pos = pos
+            self.current_char = self.text[self.pos]
+            result = self.digitpart()
+
+        result += self.exponent()
+        return result
+
+    def floatnumber(self):
+        # pointfloat | exponentfloat
+        pos = self.pos
+        try:
+            result = self.exponentfloat()
+        except CharacterError:
+            self.pos = pos
+            self.current_char = self.text[self.pos]
+            result = self.pointfloat()
+
+        return result
+
+    def imagnumber(self):
+        # (floatnumber | digitpart) ("j" | "J")
+        try:
+            result = self.floatnumber()
+        except CharacterError:
+            result = self.digitpart()
+
+        if self.current_char in ("j", "J"):
+            result += self.current_char
+            self.advance()
+            return result
+
+        self.error()
+
+    def number(self):
+        pos = self.pos
+        try:
+            return Token(FLOAT, float(self.floatnumber()))
+        except CharacterError:
+            self.pos = pos
+            self.current_char = self.text[self.pos]
+            return self.integer()
 
     def error(self):
-        raise Exception('Invalid character')
+        raise CharacterError('Invalid character')
 
     def get_next_token(self):
         while self.current_char is not None:
@@ -604,180 +765,117 @@ class Lexer(object):
             if self.current_char == '_':
                 return self._id()
 
+            if self.current_char.isdigit():
+                return self.number()
 
-class AST(object):
-    pass
+            if self.current_char == '.':
+                if self.peek().isdigit():
+                    return self.number()
 
+            if self.current_char in ('"', "'"):
+                return self.stringliteral()
 
-class BinOp(AST):
-    def __init__(self, left, op, right):
-        self.left = left
-        self.token = self.op = op
-        self.right = right
+            if self.current_char == "(":
+                return Token(LB, "(")
 
+            if self.current_char == ")":
+                return Token(RB, ")")
 
-class Num(AST):
-    def __init__(self, token):
-        self.token = token
-        self.value = token.value
+            if self.current_char == "[":
+                return Token(LSB, "[")
 
+            if self.current_char == "]":
+                return Token(RSB, "]")
 
-class Assign(AST):
-    def __init__(self, left, op, right):
-        self.left = left
-        self.token = self.op = op
-        self.right = right
+            if self.current_char == "{":
+                return Token(LCB, "{")
 
+            if self.current_char == "}":
+                return Token(RCB, "}")
 
-class Var(AST):
-    def __init__(self, token):
-        self.token = token
-        self.value = token.value
+            if self.current_char == ",":
+                return Token(COMMA, ",")
 
+            if self.current_char == ":":
+                return Token(COLON, ":")
 
-class Block(AST):
-    def __init__(self):
-        self.tokens = []
+            if self.current_char == ".":
+                return Token(DOT, ".")
 
+            if self.current_char == ";":
+                return Token(CEMI, ";")
 
-class CallFunc(AST):
-    def __init__(self, func, *attrs):
-        self.token = func
-        self.attrs = attrs
+# Operators
+# +       -       *       **      /       //      %      @
+# <<      >>      &       |       ^       ~
+# <       >       <=      >=      ==      !=
 
+# Delimiters
+# (       )       [       ]       {       }
+# ,       :       .       ;       @       =       ->
+# +=      -=      *=      /=      //=     %=      @=
+# &=      |=      ^=      >>=     <<=     **=
 
-# class Parser(object):
-#     def __init__(self, lexer):
-#         self.tokens = lexer.tokenize()
-#         self.cur_token = next(self.tokens)
+# special
+# '       "       #       \
 
-#     def error(self):
-#         raise Exception('Invalid syntax')
-
-#     def eat(self, token_type):
-#         if self.cur_token.type == token_type:
-#             self.cur_token = next(self.tokens)
-#         else:
-#             self.error()
-
-#     def build_ld(self):
-#         # 列表解析, 字典解析
-#         pass
-
-#     def stmt(self):
-#         # 赋值语句或call_func
-#         var_name = Var(self.cur_token)
-#         self.eat(ID)
-#         if self.cur_token.type == ASSIGN:
-#             op = self.cur_token
-#             self.eat(ASSIGN)
-#             if self.cur_token.type in (ID, FLOAT, INT):
-#                 right = self.expr(self.factor())
-#             elif self.cur_token.type in (LSB, LCB):
-#                 right = self.build_ltd()
-#             else:
-#                 self.error()
-#             node = Assign(var_name, op, right)
-#         elif self.cur_token.type == LB:
-#             node = self.call_func(var_name)
-#         else:
-#             self.error()
-#         return node
-
-#     def call_func(self, func_name):
-#         # 函数调用
-#         self.eat(LB)
-#         attrs = []
-#         while self.cur_token.type in (ID, INT, FLOAT):
-#             attrs.append(self.factor())
-#             if self.cur_token.type == COMMA:
-#                 self.eat(COMMA)
-#         self.eat(RB)
-#         return CallFunc(func_name, attrs)
-
-#     def expr(self, left):
-#         # 表达式, 也可以是函数调用
-#         while True:
-#             if self.cur_token.type == END:
-#                 self.eat(END)
-#                 return left
-#             if self.cur_token.type == RB:
-#                 return left
-#             if self.cur_token.type in (ADD, SUB):
-#                 op = self.cur_token
-#                 self.eat(op.type)
-#                 right = self.term(self.factor())
-#                 left = BinOp(left, op, right)
-#             elif self.cur_token.type in (MUL, DIV, REAL_DIV):
-#                 left = self.term(left)
-#             elif self.cur_token.type == LB:
-#                 node = self.call_func(left)
-#                 return node
-#             else:
-#                 self.error()
-#         return left
-
-#     def factor(self):
-#         # NUM或ID
-#         node = self.cur_token
-#         if node.type == ID:
-#             self.eat(ID)
-#             return Var(node)
-#         if node.type == INT:
-#             self.eat(INT)
-#             return Num(node)
-#         if node.type == FLOAT:
-#             self.eat(FLOAT)
-#             return Num(node)
-#         if node.type == LB:
-#             self.eat(LB)
-#             left = self.factor()
-#             node = self.expr(left)
-#             self.eat(RB)
-#             return node
-#         self.error()
-
-#     def term(self, left):
-#         # 乘除
-#         if self.cur_token.type in (ADD, SUB):
-#             return left
-#         if self.cur_token.type in (MUL, DIV, REAL_DIV):
-#             op = self.cur_token
-#             self.eat(op.type)
-#             right = self.factor()
-#             return BinOp(left, op, right)
-#         self.error()
-
-#     def frame(self):
-#         # 函数定义
-#         pass
-
-#     def block(self):
-#         node = Block()
-#         while True:
-#             if self.cur_token.type == END:
-#                 self.eat(END)
-#             elif self.cur_token.type == EOF:
-#                 break
-#             elif self.cur_token.type == ID:
-#                 node.tokens.append(self.stmt())
-#             elif self.cur_token.type == IF:
-#                 node.tokens.append(self.if_expr())
-#             elif self.cur_token.type == DEF:
-#                 node.tokens.append(self.frame())
-#             else:
-#                 self.error()
-#         return node
-
-#     def parse(self):
-#         node = self.block()
-#         return node
+# error
+# $       ?       `
 
 
 def main():
-    with open('test.py', "r", encoding="utf8") as f:
-        for token in Lexer(f).tokenize():
-            print(token)
+    # with open('test.py', "r", encoding="utf8") as f:
+    #     for token in Lexer(f).tokenize():
+    #         print(token)
+    test = """
+r\"hello\"
+u\"hello\"
+R\"hello\"
+U\"hello\"
+f\"hello\"
+F\"hello\"
+fr\"hello\"
+Fr\"hello\"
+fR\"hello\"
+FR\"helo\"
+rf\"hello\"
+rF\"hello\"
+Rf'hello'
+RF'hello'
+b\"hello\"
+B\"\"\"hello
+hello\"\"\"
+br'hello'
+Br'hello'
+bR'''hello
+hello'''
+'hello'
+"hello"
+"hello\b"
+\"\"\"hello\r
+hello\"\"\"
+'''hello
+hello'''
+2147483647
+0o177    
+0b100110111
+0xdeadbeef
+100_000_000_000
+0b_1110_0101
+3.14
+10.
+.001
+1e100
+3.14e-10
+0e0
+3.14_15_93
+"""
+    lexer = Lexer(test)
+    while True:
+        token = lexer.get_next_token()
+        if token is None:
+            break
+        print(token)
 
 
 if __name__ == '__main__':
