ac795d85d01f8a5eb842fbb860c2b3f44dfc7f37
aiyane <2310091880@qq.com>
2018-04-11 16:51:24 +0800

    修改可视化程序

--- dot.py
+++ dot.py
@@ -13,7 +13,7 @@ class VisitNode(object):
 
     def result(self):
         self.visit_file_input()
-        return 'digraph G {\n    node [shape=circle, fontsize=12, fontname="Courier", height=.1];\n    ranksep=.3;\n    edge [arrowsize=.5]\n' + self.content + '}'
+        return 'digraph astgraph {\n    node [shape=circle, fontsize=12, fontname="Courier", height=.1];\n    ranksep=.3;\n    edge [arrowsize=.5]\n' + self.content + '}'
 
     def visit_file_input(self):
 
@@ -25,64 +25,75 @@ class VisitNode(object):
 
     def visit_BinOp(self, root, node):
         self.num += 1
-        root2 = ''.join(['"', str(node.op.value), "_", str(self.num), '"'])
-
-        self.content += "    {} -> {}\n".format(root, root2)
+        node_token = ''.join(['node', str(self.num)])
+        root2 = ''.join(
+            [node_token, ' [label="', str(node.op.value), ':BinOp"]'])
+        self.content += ' '*4+root2 + '\n'
+        self.content += "    {} -> {}\n".format(root, node_token)
         if node.left:
-            self.visit(root2, node.left)
+            self.visit(node_token, node.left)
 
         if node.right:
-            self.visit(root2, node.right)
+            self.visit(node_token, node.right)
 
     def visit_UnaryOp(self, root0, node):
         self.num += 1
-        root = ''.join(['"', str(node.op.value), "_", str(self.num), '"'])
-        self.content += "    {} -> {}\n".format(root0, root)
+        node_token = ''.join(['node', str(self.num)])
+        root = ''.join(
+            [node_token, ' [label="', str(node.op.value), ':UnaryOp"]'])
+        self.content += ' '*4+root + '\n'
+        self.content += "    {} -> {}\n".format(root0, node_token)
 
         if node.token:
-            self.visit(root, node.token)
+            self.visit(node_token, node.token)
 
     def visit_ListObj(self, root, node):
         for token in node.tokens:
             self.visit(root, token)
 
     def visit_Token(self, root, node):
-        if hasattr(node, 'value'):
-            self.num += 1
-            token = ''.join(['"', str(node.value), '_', str(self.num), '"'])
-            self.content += "    {} -> {}\n".format(root, token)
-        else:
-            self.visit(token, node)
+        self.num += 1
+        node_token = ''.join(['node', str(self.num)])
+        token = ''.join([node_token, ' [label="', str(node.value), '"]'])
+        self.content += ' '*4 + token + '\n'
+        self.content += "    {} -> {}\n".format(root, node_token)
 
     def visit_IfExpr(self, root, node):
         self.num += 1
-        token = ''.join(['"IfExpr_', str(self.num), '"'])
-        self.content += '    {} -> {}\n'.format(root, token)
+        node_token = ''.join(['node', str(self.num)])
+        token = ''.join([node_token, ' [label="IfExpr', '"]'])
+        self.content += ' '*4+token + '\n'
+        self.content += '    {} -> {}\n'.format(root, node_token)
         for item in node.tokens:
-            self.visit(token, item)
+            self.visit(node_token, item)
 
     def visit_IfItem(self, root, node):
         self.num += 1
+        node_token = ''.join(['node', str(self.num)])
         if node.condition:
-            token = ''.join(['"IfItem_', str(self.num), '"'])
-            self.content += '    {} -> {}\n'.format(root, token)
-            self.visit(token, node.condition)
+            token = ''.join([node_token, ' [label="IfItem', '"]'])
+            self.content += ' '*4 + token + '\n'
+            self.content += '    {} -> {}\n'.format(root, node_token)
+            self.visit(node_token, node.condition)
         else:
-            token = ''.join(['"Else_', str(self.num), '"'])
-            self.content += '    {} -> {}\n'.format(root, token)
-        self.visit(token, node.stmt)
+            token = ''.join([node_token, ' [label="Else', '"]'])
+            self.content += ' '*4 + token + '\n'
+            self.content += '    {} -> {}\n'.format(root, node_token)
+        self.visit(node_token, node.stmt)
 
     def visit_ThreeOp(self, root, node):
         self.num += 1
-        token = ''.join(['"', node.op.value, '_', str(self.num), '"'])
-        self.content += "    {} -> {}\n".format(root, token)
+        node_token = ''.join(['node', str(self.num)])
+        token = ''.join([node_token, ' [label="', node.op.value, ':ThreeOp"]'])
+        self.content += ' '*4 + token + '\n'
+        self.content += "    {} -> {}\n".format(root, node_token)
 
         if node.left:
-            self.visit(token, node.left)
+            self.visit(node_token, node.left)
         if node.middle:
-            self.visit(token, node.middle)
+            self.visit(node_token, node.middle)
         if node.right:
-            self.visit(token, node.right)
+            self.visit(node_token, node.right)
 
     def visit(self, root, node):
         method_name = 'visit_' + type(node).__name__
